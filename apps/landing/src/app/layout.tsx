e7f8132d316b2c1122c3fd709fb55dd97f5af8d1
criskian <1044213052@u.icesi.edu.co>
2026-02-11 12:04:21 -0500

    feat: google analytics

--- apps/landing/src/app/layout.tsx
+++ apps/landing/src/app/layout.tsx
@@ -28,6 +28,22 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        {/* Google Tag Manager - GTM-PV2PJSFH */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PV2PJSFH');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Meta Pixel Code - Facebook/Instagram Ads Tracking - Pixel ID: 1548507996412660 */}
         <Script
           id="meta-pixel"
@@ -58,7 +74,19 @@ export default function RootLayout({
         </noscript>
         {/* End Meta Pixel Code */}
       </head>
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PV2PJSFH"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   );
 }
